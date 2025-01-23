b633fb547b8e1fec0b8288b095c90ea2b356f9ea
Kenneth L. Perez <148478983+PolongDev@users.noreply.github.com>
2025-01-23 09:22:13 +0800

    Update honeycomb.tsx

--- src/components/honeycomb.tsx
+++ src/components/honeycomb.tsx
@@ -7,24 +7,6 @@ interface _props {
 function HoneyComb(props: _props) {
   return (
     <div className={`flex ${props.className}`}>
-      <Hexagon className="relative top-20" src="ai-haibara.png">
-        AI Haibara
-      </Hexagon>
-      <Hexagon className="relative left-[-2rem]" src="aria-bot.png">
-        Aria Bot
-      </Hexagon>
-      <Hexagon
-        className="relative top-20 left-[-4rem]"
-        src="baybayin-keyboard.png"
-      >
-        Baybayin Keyboard
-      </Hexagon>
-      <Hexagon className="relative top-40 left-[-22rem] mt-2" src="chatapp.png">
-        Chat Application
-      </Hexagon>
-      <Hexagon
-        className="relative top-60 left-[-24rem] mt-2"
-        src="web-samples.png"
       >
         Web Sample
       </Hexagon>
