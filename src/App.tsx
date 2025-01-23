049e9f1a42ac8fb2d84ef5bdb5b43b27a4f4847c
Kenneth L. Perez <148478983+PolongDev@users.noreply.github.com>
2025-01-23 09:03:21 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import About from "./pages/about";
 import Contact from "./pages/contacts";
 import Hero from "./pages/hero";
-import Projects from "./pages/projects";
 import Header from "./parts/header";
 
 function App() {
@@ -16,10 +15,6 @@ function App() {
         <About
           id="about"
           className="dark:bg-[#212129] dark:text-white flex flex-col box-border items-center justify-center h-full w-full"
-        />
-        <Projects
-          id="projects"
-          className="dark:bg-[#212129] overflow-hidden dark:text-white flex flex-col box-border items-center justify-center h-full w-full"
         />
         <Contact
           id="contact"
